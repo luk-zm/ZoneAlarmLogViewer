e584ddf1e6a62f6c4e53b1e42bed12e119650d7d
Łukasz Zmuda <lukasz.zmuda@protonmail.com>
2025-03-27 13:33:14 +0100

    Move file processing to a seperate folder, fix project file

--- ZoneAlarmLogViewer/Form1.cs
+++ ZoneAlarmLogViewer/Form1.cs
@@ -18,88 +18,7 @@ namespace import_danych
     public partial class Form1: Form
     {
 
-        public enum LINE_RESULT
-        {
-            BAD_NUM_OF_ELEMENTS,
-            BAD_TYPE,
-            OK
-        }
-
-        public LINE_RESULT validateLine(string line)
-        {
-            int commaCount = 0;
-            for (int i = 0; i < line.Length; ++i)
-            {
-                if (line[i] == ',')
-                    commaCount++;
-            }
-            if (commaCount != 5)
-                return LINE_RESULT.BAD_NUM_OF_ELEMENTS;
-            int firstCommaIndex = line.IndexOf(',');
-            string first_element = line.Substring(0, firstCommaIndex);
-            if (first_element != "FWIN" && first_element != "PE" && first_element != "ZLUpdate")
-                return LINE_RESULT.BAD_TYPE;
-            return LINE_RESULT.OK;
-        }
-
-        public string[] extractLineElements(string line)
-        {
-            if (validateLine(line) != LINE_RESULT.OK)
-            {
-                return null;
-            }
-
-            string[] result = new string[6];
-            int resultIdx = 0;
-            int elementIdx = 0;
-            int commaIdx = line.IndexOf(',');
-            while (commaIdx > -1)
-            {
-                result[resultIdx++] = line.Substring(elementIdx, commaIdx - elementIdx);
-                elementIdx = commaIdx + 1;
-                commaIdx = line.IndexOf(',', elementIdx);
-            }
-            result[resultIdx] = line.Substring(elementIdx);
-            return result;
-        }
-
-        public (List<string>[] data, int processedLinesCount) processFile(string fileName)
-        {
-            List<string>[] processedData = new List<string>[7];
-            int processedLinesCount = 0;
-            for (int i = 0; i < processedData.Length; ++i)
-            {
-                processedData[i] = new List<string>();
-            }
-            try
-            {
-                using (StreamReader sr = new StreamReader(fileName))
-                {
-                    string line;
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        string[] elements = extractLineElements(line);
-                        if (elements != null)
-                        {
-                            processedData[1].Add(elements[0]);
-                            processedData[2].Add(elements[1]);
-                            processedData[3].Add(elements[2]);
-                            processedData[4].Add(elements[3]);
-                            processedData[5].Add(elements[4]);
-                            processedData[6].Add(elements[5]);
-                            processedLinesCount++;
-                        }
-                    }
-                }
-            }
-            catch (Exception exp)
-            {
-                Console.WriteLine("The file could not be read:");
-                Console.WriteLine(exp.Message);
-            }
-            processedData[0].Add(fileName);
-            return (processedData, processedLinesCount);
-        }
+       
 
         public void updateViews(List<string>[] data)
         {
@@ -271,7 +190,7 @@ namespace import_danych
         private void fileLoadButton_Click(object sender, EventArgs e)
         {
             string fileName = fileNameTextBox.Text;
-            (List<string>[] data, int processedLinesCount) = processFile(fileName);
+            (List<string>[] data, int processedLinesCount) = fileProcessing.processFile(fileName);
             this.data = data;
             processedLinesCountLabel.Text = "Przetworzone linijki: " + processedLinesCount;
             listView.VirtualListSize = data[1].Count;
@@ -301,91 +220,16 @@ namespace import_danych
             aboutForm.Show(this);
         }
 
-        public void processFolderThread(ref string[] files, int startIdx, int endIdx, ref List<string>[] result, ref int processedLinesCount)
-        {
-            result = new List<string>[7];
-            for (int i = 0; i < result.Length; ++i)
-            {
-                result[i] = new List<string>();
-            }
-            for (; startIdx < endIdx; ++startIdx)
-            {
-                if (!files[startIdx].EndsWith(".txt"))
-                    continue;
-                (List<string>[] processingResult, int processedLinesOfFileCount) = processFile(files[startIdx]);
-                processedLinesCount += processedLinesOfFileCount;
-                for (int j = 0; j < result.Length; ++j)
-                {
-                    for (int i = 0; i < processingResult[j].Count; ++i)
-                    {
-                        result[j].Add(processingResult[j][i]);
-                    }
-                }
-            }
-        }
-
-        public (List<string>[] data, int processedLinesCount) processFolder(string folderName)
-        {
-            List<string>[] result = new List<string>[7];
-            for (int i = 0; i < result.Length; ++i)
-            {
-                result[i] = new List<string>();
-            }
-            string[] files = Directory.GetFiles(folderName);
-            
-            int numOfThreads = 6;
-            Thread[] threads = new Thread[numOfThreads];
-            List<string>[][] tempResults = new List<string>[numOfThreads][];
-            int[] processedLinesCounts = new int[numOfThreads];
-            
-            int step = (int)Math.Ceiling((double)(files.Length) / numOfThreads);
-
-            for (int i = 0; i < numOfThreads - 1; ++i)
-            {
-                int threadIndex = i; // C# lambda capture
-                threads[threadIndex] = new Thread(() => processFolderThread(ref files, step * threadIndex,
-                    step * (threadIndex + 1), ref tempResults[threadIndex], ref processedLinesCounts[threadIndex]));
-                threads[threadIndex].Start();
-            }
-            threads[numOfThreads - 1] = new Thread(() =>
-                processFolderThread(ref files, (numOfThreads - 1)*step, files.Length,
-                ref tempResults[numOfThreads - 1], ref processedLinesCounts[numOfThreads - 1]));
-            threads[numOfThreads - 1].Start();
-
-            int processedLinesCount = 0;
-            for (int i = 0; i < numOfThreads; ++i)
-            {
-                threads[i].Join();
-            }
-            for (int i = 0; i < numOfThreads; ++i)
-            {
-                for (int dataCol = 0; dataCol < result.Length; ++dataCol)
-                {
-                    for (int j = 0; j < tempResults[i][dataCol].Count; ++j)
-                        result[dataCol].Add(tempResults[i][dataCol][j]);
-                }
-                processedLinesCount += processedLinesCounts[i];
-            }
-            return (result, processedLinesCount);
-        }
+       
 
         private void openCatalogButton_Click(object sender, EventArgs e)
         {
             FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
-            folderBrowserDialog.SelectedPath = "C:\\Users\\Luk\\source\\repos\\import_danych\\import_danych\\dane\\";
+            folderBrowserDialog.SelectedPath = @"D:\rizzai\ZoneAlarmLogViewer\ZoneAlarmLogViewer\dane";
             if (folderBrowserDialog.ShowDialog(this) == DialogResult.OK)
             {
                 string folderName = folderBrowserDialog.SelectedPath;
-                /*string[] files = Directory.GetFiles(folderName);
 
-                foreach(string file in files)
-                {
-                    List<string>[] data1 = processFile(file);
-                    appendViews(data1);
-                }*/
-
-                TimeSpan ts;
-                Stopwatch s = Stopwatch.StartNew();
                 var bw = new BackgroundWorker();
                 bw.WorkerReportsProgress = true;
                 bw.WorkerSupportsCancellation = true;
@@ -393,25 +237,13 @@ namespace import_danych
                 bw.DoWork += new DoWorkEventHandler(bw_DoWork);
                 bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bw_RunWorkerCompleted);
                 bw.RunWorkerAsync(folderName);
-
-                /*(List<string>[] data, int processedLinesCount) = processFolder(folderName);
-                processedLinesCountLabel.Text = "Przetworzone linijki: " + processedLinesCount;
-                this.data = data;
-                ts = s.Elapsed;
-                Console.WriteLine(ts);
-                listView.VirtualListSize = data[1].Count;
-                listView.Refresh();
-                fullFileListView.VirtualListSize = data[0].Count;
-                fullFileListView.Refresh();
-                ts = s.Elapsed - ts;
-                Console.WriteLine(ts);*/
             }
         }
         void bw_DoWork(object sender, DoWorkEventArgs e)
         {
             
             string folderName = (string)e.Argument;
-            e.Result = processFolder(folderName);
+            e.Result = fileProcessing.processFolder(folderName);
 
         }
 
---
+++ ZoneAlarmLogViewer/fileProcessing.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace import_danych
+{
+    internal class fileProcessing
+    {
+        public enum LINE_RESULT
+        {
+            BAD_NUM_OF_ELEMENTS,
+            BAD_TYPE,
+            OK
+        }
+
+        static public LINE_RESULT validateLine(string line)
+        {
+            int commaCount = 0;
+            for (int i = 0; i < line.Length; ++i)
+            {
+                if (line[i] == ',')
+                    commaCount++;
+            }
+            if (commaCount != 5)
+                return LINE_RESULT.BAD_NUM_OF_ELEMENTS;
+            int firstCommaIndex = line.IndexOf(',');
+            string first_element = line.Substring(0, firstCommaIndex);
+            if (first_element == "type")
+                return LINE_RESULT.BAD_TYPE;
+            return LINE_RESULT.OK;
+        }
+
+        static public string[] extractLineElements(string line)
+        {
+            if (validateLine(line) != LINE_RESULT.OK)
+            {
+                return null;
+            }
+
+            string[] result = new string[6];
+            int resultIdx = 0;
+            int elementIdx = 0;
+            int commaIdx = line.IndexOf(',');
+            while (commaIdx > -1)
+            {
+                result[resultIdx++] = line.Substring(elementIdx, commaIdx - elementIdx);
+                elementIdx = commaIdx + 1;
+                commaIdx = line.IndexOf(',', elementIdx);
+            }
+            result[resultIdx] = line.Substring(elementIdx);
+            return result;
+        }
+
+        static public(List<string>[] data, int processedLinesCount) processFile(string fileName)
+        {
+            List<string>[] processedData = new List<string>[7];
+            int processedLinesCount = 0;
+            for (int i = 0; i < processedData.Length; ++i)
+            {
+                processedData[i] = new List<string>();
+            }
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] elements = extractLineElements(line);
+                        if (elements != null)
+                        {
+                            processedData[1].Add(elements[0]);
+                            processedData[2].Add(elements[1]);
+                            processedData[3].Add(elements[2]);
+                            processedData[4].Add(elements[3]);
+                            processedData[5].Add(elements[4]);
+                            processedData[6].Add(elements[5]);
+                            processedLinesCount++;
+                        }
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(exp.Message);
+            }
+            processedData[0].Add(fileName);
+            return (processedData, processedLinesCount);
+        }
+
+        static public void processFolderThread(ref string[] files, int startIdx, int endIdx, ref List<string>[] result, ref int processedLinesCount)
+        {
+            result = new List<string>[7];
+            for (int i = 0; i < result.Length; ++i)
+            {
+                result[i] = new List<string>();
+            }
+            for (; startIdx < endIdx; ++startIdx)
+            {
+                if (!files[startIdx].EndsWith(".txt"))
+                    continue;
+                (List<string>[] processingResult, int processedLinesOfFileCount) = processFile(files[startIdx]);
+                processedLinesCount += processedLinesOfFileCount;
+                for (int j = 0; j < result.Length; ++j)
+                {
+                    for (int i = 0; i < processingResult[j].Count; ++i)
+                    {
+                        result[j].Add(processingResult[j][i]);
+                    }
+                }
+            }
+        }
+
+        static public(List<string>[] data, int processedLinesCount) processFolder(string folderName)
+        {
+            List<string>[] result = new List<string>[7];
+            for (int i = 0; i < result.Length; ++i)
+            {
+                result[i] = new List<string>();
+            }
+            string[] files = Directory.GetFiles(folderName);
+
+            int numOfThreads = 6;
+            Thread[] threads = new Thread[numOfThreads];
+            List<string>[][] tempResults = new List<string>[numOfThreads][];
+            int[] processedLinesCounts = new int[numOfThreads];
+
+            int step = (int)Math.Ceiling((double)(files.Length) / numOfThreads);
+
+            for (int i = 0; i < numOfThreads - 1; ++i)
+            {
+                int threadIndex = i; // C# lambda capture
+                threads[threadIndex] = new Thread(() => processFolderThread(ref files, step * threadIndex,
+                    step * (threadIndex + 1), ref tempResults[threadIndex], ref processedLinesCounts[threadIndex]));
+                threads[threadIndex].Start();
+            }
+            threads[numOfThreads - 1] = new Thread(() =>
+                processFolderThread(ref files, (numOfThreads - 1) * step, files.Length,
+                ref tempResults[numOfThreads - 1], ref processedLinesCounts[numOfThreads - 1]));
+            threads[numOfThreads - 1].Start();
+
+            int processedLinesCount = 0;
+            for (int i = 0; i < numOfThreads; ++i)
+            {
+                threads[i].Join();
+            }
+            for (int i = 0; i < numOfThreads; ++i)
+            {
+                for (int dataCol = 0; dataCol < result.Length; ++dataCol)
+                {
+                    for (int j = 0; j < tempResults[i][dataCol].Count; ++j)
+                        result[dataCol].Add(tempResults[i][dataCol][j]);
+                }
+                processedLinesCount += processedLinesCounts[i];
+            }
+            return (result, processedLinesCount);
+        }
+    }
+}
